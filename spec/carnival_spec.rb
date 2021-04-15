90359cffa25c833e71247becf1019e5b1a0ec287
NoahZinter <noah.zinter@gmail.com>
2021-04-15 09:49:21 -0600

    Create attendees_by_ride_interest spec

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -77,5 +77,35 @@ describe Carnival do
       expect(jeffco_fair.attendees).to eq([bob, sally, johnny])
     end
   end
+
+  describe '#attendees by ride interest' do
+    it 'returns a Hash with Rides as keys and interested attendees as values' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+      ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+      bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+      scrambler = Ride.new({name: 'Scrambler', cost: 15})
+      jeffco_fair.add_ride(ferris_wheel)
+      jeffco_fair.add_ride(bumper_cars)
+      jeffco_fair.add_ride(scrambler)
+
+      bob = Attendee.new('Bob', 20)
+      sally = Attendee.new('Sally', 20)
+      johnny = Attendee.new('Johnny', 5)
+      bob.add_interest('Ferris Wheel')
+      bob.add_interest('Bumper Cars')
+      sally.add_interest('Scrambler')
+      johnny.add_interest('Bumper Cars')
+
+      jeffco_fair.admit(bob)
+      jeffco_fair.admit(sally)
+      jeffco_fair.admit(johnny)
+
+      expect(jeffco_fair.attendees_by_ride_interest).to eq({
+          ferris_wheel => [bob],
+          bumper_cars => [bob, johnny],
+          scrambler => [sally]
+                })
+    end
+  end
 end
 
