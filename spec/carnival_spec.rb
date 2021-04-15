d4e01271222dca0f0a5f6ac7422591a8f74fb0ce
NoahZinter <noah.zinter@gmail.com>
2021-04-15 09:26:50 -0600

    Create add_ride spec

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -15,5 +15,20 @@ describe Carnival do
     expect(jeffco_fair.name).to eq("Jefferson County Fair")
     expect(jeffco_fair.rides).to eq([])
   end
+
+  describe '#add ride' do
+    it 'adds rides to @rides' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+      ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+      bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+      scrambler = Ride.new({name: 'Scrambler', cost: 15})
+
+      jeffco_fair.add_ride(ferris_wheel)
+      jeffco_fair.add_ride(bumper_cars)
+      jeffco_fair.add_ride(scrambler)
+
+      expect(jeffco_fair.rides).to eq([ferris_wheel, bumper_cars, scrambler])
+    end
+  end
 end
 
