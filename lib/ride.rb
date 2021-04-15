e47ef7874bc8ef04e0485a3178b549c09510cbef
NoahZinter <noah.zinter@gmail.com>
2021-04-15 09:08:01 -0600

    Test Ride attributes

---
+++ lib/ride.rb
@@ -0,0 +1,6 @@
+class Ride
+  def initialize(details)
+    @name = details[:name]
+    @cost = details[:cost]
+  end
+end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -6,4 +6,11 @@ describe Ride do
 
     expect(ride).is_a? Ride 
   end
+
+  it 'has attributes' do 
+    ride = Ride.new({name: 'Ferris Wheel', cost: 0})
+
+    expect(ride.name).to eq('Ferris Wheel')
+    expect(ride.cost).to eq(0)
+  end 
 end 
