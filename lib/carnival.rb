257b18e506946b2b7e3f54b30ac883d01740f19c
NoahZinter <noah.zinter@gmail.com>
2021-04-15 09:33:10 -0600

    Pass recommend rides spec

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -8,4 +8,10 @@ class Carnival
   def add_ride(ride)
     @rides << ride
   end
+
+  def recommend_rides(attendee)
+    @rides.select do |ride|
+      attendee.interests.include?(ride.name)
+    end
+  end
 end
