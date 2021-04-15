da12a941269481b082ad7642ed6e572cd4d60c3c
NoahZinter <noah.zinter@gmail.com>
2021-04-15 09:15:54 -0600

    Add interests array

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -13,5 +13,15 @@ describe Attendee do
     expect(attendee.name).to eq('Bob')
     expect(attendee.spending_money).to eq(20)
   end
+
+  it 'starts with an empty array of interests' do
+    attendee = Attendee.new('Bob', 20)
+
+    expect(attendee.interests).to eq([])
+  end
+
+  describe '#add interest' do
+
+  end
 end
 
