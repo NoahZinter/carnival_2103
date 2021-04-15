de443d4f52527b77448db10bcbdfe688810b3ae1
NoahZinter <noah.zinter@gmail.com>
2021-04-15 09:40:24 -0600

    Create admit and @attendees spec

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -14,6 +14,7 @@ describe Carnival do
 
     expect(jeffco_fair.name).to eq("Jefferson County Fair")
     expect(jeffco_fair.rides).to eq([])
+    expect(jeffco_fair.attendees).to eq([])
   end
 
   describe '#add ride' do
@@ -49,6 +50,25 @@ describe Carnival do
 
       expect(jeffco_fair.recommend_rides(bob)).to eq([ferris_wheel, bumper_cars])
       expect(jeffco_fair.recommend_rides(sally)).to eq([scrambler])
+    end
+  end
+
+  describe '#admit' do
+   xit 'adds attendees to new @attendees instance var' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+      ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+      bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+      scrambler = Ride.new({name: 'Scrambler', cost: 15})
+      jeffco_fair.add_ride(ferris_wheel)
+      jeffco_fair.add_ride(bumper_cars)
+      jeffco_fair.add_ride(scrambler)
+
+      bob = Attendee.new('Bob', 20)
+      sally = Attendee.new('Sally', 20)
+      johnny = Attendee.new('Johnny', 5)
+      bob.add_interest('Ferris Wheel')
+      bob.add_interest('Bumper Cars')
+      sally.add_interest('Scrambler')
     end
   end
 end
